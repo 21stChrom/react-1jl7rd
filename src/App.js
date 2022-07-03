45ab1c58898aa9787cdddc541a58af832c1d989c
21stChrom <parkerjmartin2020@gmail.com>
2022-07-02 21:18:42 -0400

    1 binaries uploaded

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
-      <audio controls src="Juice WRLD - No Laces (Unreleased) (Lyrics) [720p].mp3" className="audio"></audio>
+      <audio controls  className="audio">
+        <source src="audio/Juice mus/Juice WRLD - No Laces (Unreleased) (Lyrics) [720p].mp3" type="audio/mpeg" />
+        <source src="Juice WRLD - No Laces (Unreleased) (Lyrics) [720p].mp3" type="audio/mpeg" />
+      </audio>
       <p>Start editing to see some magic happen :)</p>
     </div>
   );
