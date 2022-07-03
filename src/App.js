1a55521ec59f2f5de6d23063a21edefee7a0d42c
21stChrom <parkerjmartin2020@gmail.com>
2022-07-02 21:06:01 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <audio controls src="Juice WRLD - No Laces (Unreleased) (Lyrics) [720p].mp3" className="audio"></audio>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
